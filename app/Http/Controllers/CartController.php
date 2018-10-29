4e63ad1ecdebddb675d9116c951e9a28bb0d254c
laureanopiotti <laureanopiotti@gmail.com>
2018-10-29 20:22:20 -0300

    Array products has Product Model with stock as an attribute

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,12 +9,13 @@ class CartController extends Controller
 {
     public function index()
     {
-        $products = session('cart.products');
+        $session_products = session('cart.products');
+        $products = [];
 
-
-        foreach ($products as $key => $value) {
-            $products[$key]['product'] = Product::find($products[$key]['id']);
-            unset($products[$key]['id']);
+        foreach ($session_products as $key => $value) {
+            $product = Product::find($session_products[$key]['id']);
+            $product->stock = $session_products[$key]['stock'];
+            $products[] = $product; 
         }
    
         return view('cart')->with('products', $products);
