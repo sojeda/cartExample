fd85fdeb2aadc6d0ec358ee9d3bc19a1873bebb8
laureanopiotti <laureanopiotti@gmail.com>
2018-10-29 20:06:22 -0300

    Variable products is an array of Product Model and its stock

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class CartController extends Controller
 {
     public function index()
     {
         $products = session('cart.products');
-        
+
+
+        foreach ($products as $key => $value) {
+            $products[$key]['product'] = Product::find($products[$key]['id']);
+            unset($products[$key]['id']);
+        }
+   
         return view('cart')->with('products', $products);
 
     }
