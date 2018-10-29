9759e2ce52f29195f8d2dc2b280665eebdf45351
Lucas Emiliani <lucasemiliani1@hotmail.com>
2018-10-29 20:04:44 -0300

    agregda la nav al carrito

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -96,3 +99,4 @@
         </div>
     </div>
 </div>
+@endsection
